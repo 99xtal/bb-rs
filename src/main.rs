d395b4a614cb3cd05aed03f58c50df9a97a8fc21
jrybarczyk <jrybarczyk@exclusiveresorts.com>
2023-03-12 19:10:24 -0400

    Add command descriptions

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ mod bitbucket_repo;
 #[command(
     author,
     version,
-    about = "An unofficial command-line tool for working with BitBucket."
+    about = "An unofficial command-line tool for interacting with BitBucket repositories."
 )]
 struct Cli {
     #[command(subcommand)]
@@ -20,22 +20,29 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
+    /// Open the repository in the browser
     Browse {
+        /// Select another branch by passing in the branch name
         #[arg(value_name = "branch", short, long)]
         branch: Option<String>,
 
+        /// Open source code at a specific commit
         #[arg(value_name = "commit", short, long)]
         commit: Option<String>,
 
+        /// Print destination URL instead of opening the browser
         #[arg(value_name = "no_browser", short, long)]
         no_browser: bool,
 
+        /// Select another repository using the WORKSPACE/REPO format
         #[arg(value_name = "repo", short, long)]
         repo: Option<String>,
 
+        /// Not implemented, will be ignored
         #[arg(value_name = "settings", short, long)]
         settings: Option<String>
     },
+    /// Clone a repository locally
     Clone {
         remote: String
     }
